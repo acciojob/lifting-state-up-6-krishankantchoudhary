15c4a052e3c41bdd9e13dbbad64127e1ad0d10ac
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-11-03 11:53:30 +0530

    reupdated

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,9 @@ import { useState } from "react";
 
 const App = () => {
   const [todos, setTodos] = useState([
-    { id: 1, text: "Learn React", completed: false },
-    { id: 2, text: "Build a React app", completed: false },
-    { id: 3, text: "Deploy the React app", completed: false },
+    { id: 1, text: "Learn React:", completed: false },
+    { id: 2, text: "Build a React app:", completed: false },
+    { id: 3, text: "Deploy the React app:", completed: false },
   ]);
 
   const handleComplete = (id) => {
