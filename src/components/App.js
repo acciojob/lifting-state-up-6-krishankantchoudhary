585e05daaf9a0548bb52e5f1091e3457bb645021
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-12-06 13:15:15 +0530

    refactor

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,11 @@ function TodoApp() {
     { id: 1, text: "Learn React", completed: false },
     { id: 2, text: "Build a React app", completed: false },
     { id: 3, text: "Deploy the React app", completed: false },
-    
   ]);
 
   const handleComplete = (id) => {
-    // Update todo as completed
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) =>
+    setTodos((prev) =>
+      prev.map((todo) =>
         todo.id === id ? { ...todo, completed: true } : todo
       )
     );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,12 @@
 import React from "react";
 
 function TodoList({ todos, onComplete }) {
-  const anyCompleted = todos.some(todo => todo.completed);
-
   return (
     <ul>
       {todos.map((todo) => (
         <li key={todo.id}>
           {todo.text}
-          {!anyCompleted && (
+          {!todo.completed && (
             <button onClick={() => onComplete(todo.id)}>Complete</button>
           )}
         </li>
