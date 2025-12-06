fec41d94b90890907c716c5b697f30d833510d6b
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-12-06 13:10:34 +0530

    coder

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 function TodoList({ todos, onComplete }) {
-
-  // Check if ANY todo is completed
   const anyCompleted = todos.some(todo => todo.completed);
 
   return (
@@ -10,8 +8,6 @@ function TodoList({ todos, onComplete }) {
       {todos.map((todo) => (
         <li key={todo.id}>
           {todo.text}
-
-          {/* If ANY todo is completed, hide ALL buttons */}
           {!anyCompleted && (
             <button onClick={() => onComplete(todo.id)}>Complete</button>
           )}
